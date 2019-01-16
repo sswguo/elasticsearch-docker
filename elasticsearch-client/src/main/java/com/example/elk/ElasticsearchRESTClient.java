baf37156a328313d08799d6c70d00974ea758626
Wenjie Guo <wguo@redhat.com>
2019-01-16 16:33:00 +0800

    try boolquery

--- elasticsearch-client/src/main/java/com/example/elk/ElasticsearchRESTClient.java
+++ elasticsearch-client/src/main/java/com/example/elk/ElasticsearchRESTClient.java
@@ -10,6 +10,7 @@ import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.common.unit.TimeValue;
+import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.SearchHits;
@@ -18,72 +19,100 @@ import org.elasticsearch.search.aggregations.Aggregations;
 import org.elasticsearch.search.aggregations.bucket.terms.Terms;
 import org.elasticsearch.search.aggregations.bucket.terms.TermsAggregationBuilder;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.elasticsearch.search.sort.FieldSortBuilder;
+import org.elasticsearch.search.sort.SortOrder;
 
 public class ElasticsearchRESTClient {
 
-	public RestHighLevelClient getRestClient(){
+	public static final String TRACKING_ID = "build_002";
+
+	public RestHighLevelClient getRestClient() {
 		RestHighLevelClient client = new RestHighLevelClient(
-		        RestClient.builder(
-		                new HttpHost(ELKConstant.SERVER_HOST, 9200, "http")));
+				RestClient.builder(new HttpHost(ELKConstant.SERVER_HOST, 9200, "http")));
 		return client;
 	}
-	
-	public static Consumer < SearchHit > hitConsumer = ( hit ) -> {
+
+	public static Consumer<SearchHit> hitConsumer = (hit) -> {
 		System.out.println(hit.getSourceAsString());
 	};
-	
-	public static void main(String[] args) throws Exception{
-		//tryTermsQuery();
-		tryAggregation();
-		
+
+	public static void main(String[] args) throws Exception {
+		// tryTermsQuery();
+		// tryAggregation();
+		tryBoolquery();
+
 	}
-	
-	public static void tryTermsQuery() throws Exception{
-		try (RestHighLevelClient client = new ElasticsearchRESTClient().getRestClient()){
-			
-			SearchSourceBuilder sourceBuilder = new SearchSourceBuilder(); 
-			sourceBuilder.query(QueryBuilders.termQuery("trackingID", "build_002")); 
-			sourceBuilder.from(0); 
-			sourceBuilder.size(5); 
+
+	public static void tryTermsQuery() throws Exception {
+		try (RestHighLevelClient client = new ElasticsearchRESTClient().getRestClient()) {
+
+			SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+			sourceBuilder.query(QueryBuilders.termQuery("trackingID", TRACKING_ID));
+			sourceBuilder.from(0);
+			sourceBuilder.size(5);
 			sourceBuilder.timeout(new TimeValue(60, TimeUnit.SECONDS));
-			
+
 			SearchRequest searchRequest = new SearchRequest();
 			searchRequest.indices("indy");
 			searchRequest.source(sourceBuilder);
-			
+
 			SearchResponse searchResponse = client.search(searchRequest, RequestOptions.DEFAULT);
 			SearchHits hits = searchResponse.getHits();
-			
+
 			hits.forEach(hitConsumer);
-			
-		}catch(Exception e) {
-			
+
+		} catch (Exception e) {
+
 		}
 	}
-	
-	public static void tryAggregation() throws Exception{
-		try (RestHighLevelClient client = new ElasticsearchRESTClient().getRestClient()){
-			SearchSourceBuilder sourceBuilder = new SearchSourceBuilder(); 
-			sourceBuilder.query(QueryBuilders.termQuery("extra.trackingId", "build_002"));
+
+	public static void tryAggregation() throws Exception {
+		try (RestHighLevelClient client = new ElasticsearchRESTClient().getRestClient()) {
+			SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+			sourceBuilder.query(QueryBuilders.termQuery("extra.trackingId", TRACKING_ID));
 			sourceBuilder.size(0);
-			TermsAggregationBuilder aggregation = AggregationBuilders.terms("by_eventType")
-			        .field("eventType");
+			TermsAggregationBuilder aggregation = AggregationBuilders.terms("by_eventType").field("eventType");
 			sourceBuilder.aggregation(aggregation);
-			
+
 			SearchRequest searchRequest = new SearchRequest();
 			searchRequest.indices("audit");
 			searchRequest.source(sourceBuilder);
-			
+
 			SearchResponse searchResponse = client.search(searchRequest, RequestOptions.DEFAULT);
 			SearchHits hits = searchResponse.getHits();
-			
+
 			// Retrieving Aggregations
 			Aggregations aggregations = searchResponse.getAggregations();
 			Terms byTypeAggregation = aggregations.get("by_eventType");
-			
-			byTypeAggregation.getBuckets().forEach( bucket -> {
+
+			byTypeAggregation.getBuckets().forEach(bucket -> {
 				System.out.println(bucket.getKeyAsString() + "|" + bucket.getDocCount());
 			});
+
+			hits.forEach(hitConsumer);
+		}
+	}
+
+	public static void tryBoolquery() throws Exception {
+		try (RestHighLevelClient client = new ElasticsearchRESTClient().getRestClient()) {
+			SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+			BoolQueryBuilder queryBuilders = QueryBuilders.boolQuery()
+					.must(QueryBuilders.matchQuery("extra.trackingId", TRACKING_ID))
+					.must(QueryBuilders.matchQuery("eventType", "ACCESS"));
+
+			// sorting
+			sourceBuilder.sort(new FieldSortBuilder("timestamp").order(SortOrder.DESC));
+
+			sourceBuilder.query(queryBuilders);
+
+			sourceBuilder.timeout(new TimeValue(60, TimeUnit.SECONDS));
+
+			SearchRequest searchRequest = new SearchRequest();
+			searchRequest.indices("audit");
+			searchRequest.source(sourceBuilder);
+
+			SearchResponse searchResponse = client.search(searchRequest, RequestOptions.DEFAULT);
+			SearchHits hits = searchResponse.getHits();
 			
 			hits.forEach(hitConsumer);
 		}
